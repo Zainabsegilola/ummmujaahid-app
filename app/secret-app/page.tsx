e98054a06d2067ab022d1f3ee421dbf1c5c6a007
Zainabsegilola <160668941+Zainabsegilola@users.noreply.github.com>
2025-06-27 10:09:18 +0100

    Update page.tsx
    added import from new quran cache functions from database.js

--- app/secret-app/page.tsx
+++ app/secret-app/page.tsx
@@ -26,7 +26,8 @@ import {
   updateCardContent,
   softDeleteDeck,
   restoreDeletedDeck,
-     
+  getQuranTranslationCache,        
+  saveQuranTranslationCache  
 } from '@/lib/database'
 import { 
   fetchSurahsList, 
